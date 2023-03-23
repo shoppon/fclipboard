5870357215e324f8d9a46551fe41976021656bb0
Shoppon <shopppon@gmail.com>
2023-03-23 19:49:23 +0800

    add k8s

--- lib/matcher.dart
+++ lib/matcher.dart
@@ -29,14 +29,14 @@ class Matcher {
   }
 
   List<Entry> match(String leading) {
-    final keys = all.map((e) => e.title).toList();
-    final fuse =
-        Fuzzy(keys, options: FuzzyOptions(distance: 80, maxPatternLength: 10));
+    final keys = all.map((e) => e.title.replaceAll(RegExp("_"), "")).toList();
+    final fuse = Fuzzy(keys, options: FuzzyOptions(threshold: 0.8));
     var result = fuse.search(leading);
     result = result.sublist(0, min(maxMatches, result.length));
     List<Entry> matches = [];
     for (var r in result) {
-      matches.add(all.firstWhere((e) => e.title == r.item));
+      matches.add(
+          all.firstWhere((e) => e.title.replaceAll(RegExp("_"), "") == r.item));
     }
     return matches;
   }
