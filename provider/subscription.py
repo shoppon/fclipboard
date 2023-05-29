c1b85b3567d977ddf2940f3df3c182f196ab19b0
Shoppon <shopppon@gmail.com>
2023-05-29 10:53:52 +0800

    add provider

---
+++ provider/subscription.py
@@ -0,0 +1,26 @@
+import argparse
+import yaml
+
+import uvicorn
+from fastapi import FastAPI
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--file", type=str, default="subscription.yaml")
+args = parser.parse_args()
+
+app = FastAPI()
+
+
+@app.get("/subscription")
+def get_subscription():
+    with open(args.file, encoding='utf-8') as f:
+        content = yaml.safe_load(f)
+        return content
+
+
+def main():
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+if __name__ == "__main__":
+    main()
