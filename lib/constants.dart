6606f6c015f7e85394604590574283fb9a36dee9
Shoppon <shopppon@gmail.com>
2023-09-12 16:53:37 +0800

    deploy web

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,3 @@
 // const baseURL = "http://127.0.0.1:8000";
-const baseURL = "http://fclipboard.shoppon.club:8888";
+const baseURL = "http://fclipboard-provider.shoppon.club:8888";
 const defaultEmail = "anonymous@fclipboard.io";
