3d486730de167b4a0147dabb19df909d9c870f60
Shoppon <shopppon@gmail.com>
2023-11-07 14:31:21 +0800

    fix build issue on new macos version

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,3 @@
 // const baseURL = "http://127.0.0.1:8000";
-const baseURL = "http://fclipboard.shoppon.xyz:8888";
+const baseURL = "http://fclipboard-provider.shoppon.xyz:8888";
 const defaultEmail = "anonymous@fclipboard.io";
